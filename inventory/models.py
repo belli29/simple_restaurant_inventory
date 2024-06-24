baf107dba8e6bf77d333b5eb96e46a7c4ef18575
belli29 <belli29@hotmail.com>
2024-06-24 15:54:50 +0200

    amended models fields

--- inventory/models.py
+++ inventory/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
-    quantity = models.IntegerField(max_length=3)
+    quantity = models.IntegerField(blank=False, null=False)
     unit_price = models.DecimalField(decimal_places=2, max_digits=3, blank=False, null=False)
     unit = models.CharField(max_length=100, null=False, blank=False)
 
@@ -13,7 +13,7 @@ class MenuItem(models.Model):
 class RecipeRequirements(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantity = models.DecimalField(decimal_places=2, max_digits=3, blank=False, null=False)
+    quantity = models.DecimalField(decimal_places=2, max_digits=10, blank=False, null=False)
     
 
 class Purchase(models.Model):
