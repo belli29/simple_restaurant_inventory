8ca072ca8cb166fb94b0c08ae81aacbce8db13e6
belli29 <belli29@hotmail.com>
2024-06-24 16:00:30 +0200

    amended Purchase model

--- inventory/models.py
+++ inventory/models.py
@@ -20,15 +20,4 @@ class Purchase(models.Model):
     timestamp = models.DateTimeField(auto_now=True)
     items = models.ManyToManyField(MenuItem)
     
-    def calculate_total_cost(self):
-        total_cost = sum(item.price for item in self.items.all())
-        return total_cost
-
-    def save(self, *args, **kwargs):
-        self.total_cost = self.calculate_total_cost()
-        super().save(*args, **kwargs)
-    
-
-
-    
     
