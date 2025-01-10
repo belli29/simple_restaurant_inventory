cec903780601693af787390ae90f2e5080c181d6
belli29 <belli29@hotmail.com>
2025-01-10 16:12:46 +0100

    added views, urls and basic templates, settings

---
+++ inventory/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import HomeView, IngredientListView, IngredientDeleteView, MenuItemListView, PurchaseListView, ProfitRevenueView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('inventory/', IngredientListView.as_view(), name='index'),
+    path('delete/<int:pk>/', IngredientDeleteView.as_view(), name='delete_ingredient'),
+    path('menu/', MenuItemListView.as_view(), name='menu'),
+    path('purchases/', PurchaseListView.as_view(), name='purchases'),
+    path('profit-revenue/', ProfitRevenueView.as_view(), name='profit_revenue'),
+]
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DeleteView, TemplateView
+from django.db.models import Sum, F
+from .models import Ingredient, MenuItem, Purchase, RecipeRequirements
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'inventory/home.html'
+
+class IngredientListView(ListView):
+    model = Ingredient
+    template_name = 'inventory/index.html'
+    context_object_name = 'ingredients'
+
+class IngredientDeleteView(DeleteView):
+    model = Ingredient
+    success_url = reverse_lazy('index')
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+class MenuItemListView(ListView):
+    model = MenuItem
+    template_name = 'inventory/menu.html'
+    context_object_name = 'menu_items'
+
+
+class PurchaseListView(ListView):
+    model = Purchase
+    template_name = 'inventory/purchases.html'
+    context_object_name = 'purchases'
+
+class ProfitRevenueView(TemplateView):
+    template_name = 'inventory/profit_revenue.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Calculate total revenue
+        total_revenue = (
+            Purchase.objects.annotate(total_price=Sum(F('items__price')))
+            .aggregate(total_revenue=Sum('total_price'))['total_revenue']
+        )
+        # Calculate total cost
+        total_cost = sum(
+            sum(ingredient.price * ingredient.quantity for ingredient in item.ingredients.all())
+            for item in MenuItem.objects.all()
+        )
+        # Calculate profit
+        profit = total_revenue - total_cost
+        context['total_revenue'] = total_revenue
+        context['total_cost'] = total_cost
+        context['profit'] = profit
+        return context
+
